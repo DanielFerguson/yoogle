2137b4767de80174c6fd106aacac094d0f2a45a5
Daniel Ferguson <thedanielfergusonkid@gmail.com>
2024-05-19 21:26:33 +1000

    added AMPLIFY_MONOREPO_APP_ROOT env

--- infra/hosting/main.tf
+++ infra/hosting/main.tf
@@ -21,12 +21,18 @@ resource "aws_amplify_app" "web" {
     enable_auto_build = true
   }
 
+  environment_variables = {
+    NODE_ENV                  = "production"
+    AMPLIFY_MONOREPO_APP_ROOT = "apps/web"
+  }
+
   build_spec = <<-EOT
     version: 0.1
     frontend:
       phases:
         preBuild:
           commands:
+            - npm install -g pnpm
             - pnpm install
         build:
           commands:
